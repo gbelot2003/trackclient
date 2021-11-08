e45f6d06af96d5e3b3f0a1b50057c72d532ddafa
Gerardo Belot <gbelot2003@hotmail.com>
2021-11-08 13:53:06 -0600

    :wrench:  Necesita fix para implementacion de Drawpad

---
+++ src/components/Drawpad.vue
@@ -0,0 +1,34 @@
+<template>
+  <Page loaded="pageLoaded">
+    <ActionBar title="NativeScript-DrawingPad" />
+    <ScrollView>
+        <Label text="Sign/Update below" textWrap="true" horizontalAlignment="center" verticalAlignment="center" marginTop="20"></Label>
+      <StackLayout>
+
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+import { topmost } from "ui/frame";
+export default {
+  data() {
+    img: "";
+  },
+
+  methods: {
+    getMyDrawing() {
+      const DrawingPad = topmost().getViewById("myDrawingPad");
+      DrawingPad.getDrawing().then((res) => {
+        console.log(res);
+      });
+    },
+
+    clearMyDrawing() {
+      const DrawingPad = topmost().getViewById("myDrawingPad");
+      DrawingPad.clearDrawing();
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'nativescript-vue'
 import App from './components/App'
 import Home from './components/Home'
+import Drawpad from './components/Drawpad'
 import store from './store'
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
-
+Vue.registerElement('DrawingPad', () => require('@nativescript-community/drawingpad'));
 
 new Vue({
   store,
-  render: h => h('frame', [h(Home)])
+  render: h => h('frame', [h(Drawpad)])
 }).$start()
