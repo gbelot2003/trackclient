580b735db69784736eb48d55ebec3b90eef14b8f
Gerardo Belot <gbelot2003@hotmail.com>
2021-11-10 09:26:56 -0600

    :rocket: Agregando librerias de scanner

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@ import Vue from 'nativescript-vue'
 import App from './components/App'
 import Home from './components/Home'
 import Drawpad from './components/Drawpad'
+import Barcode from './components/Barcode'
+
 import store from './store'
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 Vue.registerElement('DrawingPad', () => require('@nativescript-community/drawingpad').DrawingPad);
+Vue.registerElement('BarcodeScanner', () => require('nativescript-barcodescanner').BarcodeScannerView)
 
 new Vue({
   store,
-  render: h => h('frame', [h(Home)])
+  render: h => h('frame', [h(Barcode)])
 }).$start()
