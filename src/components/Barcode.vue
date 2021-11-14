210ddd576f069c898586141246d73af2451f1f94
Gerardo Belot <gbelot2003@hotmail.com>
2021-11-13 21:40:47 -0600

    🧹 CLEAN:

--- src/components/Barcode.vue
+++ src/components/Barcode.vue
@@ -42,85 +42,85 @@
 </template>
 
 <script>
-import { BarcodeScanner } from "nativescript-barcodescanner";
+  import { BarcodeScanner } from "nativescript-barcodescanner";
 
-export default {
-  data() {
-    return {
-    };
-  },
-  methods: {
-    onScanResult(evt) {
-      console.log(`onScanResult: ${evt.text} (${evt.format})`);
+  export default {
+    data() {
+      return {
+      };
     },
-    doScanWithBackCameraWithFlip() {
-      this.scan(false, true);
+    methods: {
+      onScanResult(evt) {
+        console.log(`onScanResult: ${evt.text} (${evt.format})`);
+      },
+      doScanWithBackCameraWithFlip() {
+        this.scan(false, true);
+      },
+      doScanWithFrontCameraNoFlip() {
+        this.scan(true, false);
+      },
+      scan(preferFrontCamera, showFlipCameraButton) {
+        new BarcodeScanner()
+          .scan({
+            cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
+            cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
+            message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
+            preferFrontCamera, // Android only, default false
+            showFlipCameraButton, // default false
+            showTorchButton: true, // iOS only, default false
+            torchOn: false, // launch with the flashlight on (default false)
+            resultDisplayDuration: 500, // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
+            beepOnScan: true, // Play or Suppress beep on scan (default true)
+            openSettingsIfPermissionWasPreviouslyDenied: true, // On iOS you can send the user to the settings app if access was previously denied
+            closeCallback: () => {
+              console.log("Scanner closed @ " + new Date().getTime());
+            },
+          })
+          .then((result) => {
+              console.log("--- scanned: " + result.text);
+              let that = this; 
+              that.$store.dispatch('CHANGE_CODE', result.text);
+              console.log("this keeps working");
+          }).catch(err => {
+            console.log("No scan. " + err);
+          });
+            // function (result) {
+            //   console.log("--- scanned: " + result.text);
+            //   let that = this;   
+            //   that.$store.dispatch('CHANGE_CODE', result.text);
+            //   console.log("this keeps working");
+            //   // setTimeout(function () {  
+            //   //   alert({
+            //   //     title: "Scan result",
+            //   //     message:
+            //   //       "Format: " + result.format + ",\nValue: " + result.text,
+            //   //     okButtonText: "OK",
+            //   //   });
+            //   // }, 500);
+            // },
+            // function (errorMessage) {
+            //   console.log("No scan. " + errorMessage);
+            // }
+          // );
+      },
     },
-    doScanWithFrontCameraNoFlip() {
-      this.scan(true, false);
-    },
-    scan(preferFrontCamera, showFlipCameraButton) {
-      new BarcodeScanner()
-        .scan({
-          cancelLabel: "EXIT. Also, try the volume buttons!", // iOS only, default 'Close'
-          cancelLabelBackgroundColor: "#333333", // iOS only, default '#000000' (black)
-          message: "Use the volume buttons for extra light", // Android only, default is 'Place a barcode inside the viewfinder rectangle to scan it.'
-          preferFrontCamera, // Android only, default false
-          showFlipCameraButton, // default false
-          showTorchButton: true, // iOS only, default false
-          torchOn: false, // launch with the flashlight on (default false)
-          resultDisplayDuration: 500, // Android only, default 1500 (ms), set to 0 to disable echoing the scanned text
-          beepOnScan: true, // Play or Suppress beep on scan (default true)
-          openSettingsIfPermissionWasPreviouslyDenied: true, // On iOS you can send the user to the settings app if access was previously denied
-          closeCallback: () => {
-            console.log("Scanner closed @ " + new Date().getTime());
-          },
-        })
-        .then((result) => {
-            console.log("--- scanned: " + result.text);
-            let that = this; 
-            that.$store.dispatch('CHANGE_CODE', result.text);
-            console.log("this keeps working");
-        }).catch(err => {
-          console.log("No scan. " + err);
-        });
-          // function (result) {
-          //   console.log("--- scanned: " + result.text);
-          //   let that = this;   
-          //   that.$store.dispatch('CHANGE_CODE', result.text);
-          //   console.log("this keeps working");
-          //   // setTimeout(function () {  
-          //   //   alert({
-          //   //     title: "Scan result",
-          //   //     message:
-          //   //       "Format: " + result.format + ",\nValue: " + result.text,
-          //   //     okButtonText: "OK",
-          //   //   });
-          //   // }, 500);
-          // },
-          // function (errorMessage) {
-          //   console.log("No scan. " + errorMessage);
-          // }
-        // );
-    },
-  },
-  computed:{
-    getCodes(){
-      return this.$store.getters.getCode;
+    computed:{
+      getCodes(){
+        return this.$store.getters.getCode;
+      }
     }
-  }
-};
+  };
 </script>            
 
 <style scoped>
-ActionBar {
-  background-color: #53ba82;
-  color: #ffffff;
-}
-.message {
-  vertical-align: center;
-  text-align: center;
-  font-size: 20;
-  color: #333333;
-}
+  ActionBar {
+    background-color: #53ba82;
+    color: #ffffff;
+  }
+  .message {
+    vertical-align: center;
+    text-align: center;
+    font-size: 20;
+    color: #333333;
+  }
 </style>
