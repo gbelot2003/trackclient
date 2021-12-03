7d3acc933450415ff571b0fee9d85765242f2437
Gerardo A Belot <gbelot2003@hotmail.com>
2021-12-02 20:32:36 -0600

    🚧 PROGRESS: recibe mensajes de pusher

--- src/components/Bags/BagsPackages.vue
+++ src/components/Bags/BagsPackages.vue
@@ -3,15 +3,35 @@
     <ActionBar title="Manejo de Bolsas">
       <NavigationButton android.systemIcon="ic_menu_back" @tap="barBack" />
     </ActionBar>
-    <StackLayout></StackLayout>
+    <StackLayout>
+        <Label :text="message" />
+    </StackLayout>
   </Page>
 </template>
 
 <script>
-import Home from "../Home.vue"
+import Home from "../Home.vue";
+import Pusher from "pusher-nativescript";
+const PUSHER_APP_KEY = "d4c139db1ff2d4db8cc3";
+const PUSHER_APP_CLUSTER = "mt1";
 
 export default {
   name: "BagsPackages",
+  data(){
+    return {
+        message: 'aa',
+        channelNumber: 85,
+    }
+  },
+  created() {
+    const socket = new Pusher(PUSHER_APP_KEY, {
+      cluster: PUSHER_APP_CLUSTER
+    });
+
+    const channel = socket.subscribe("test-channel-85");
+    
+    channel.bind("NewMessage",  (data) =>  this.message = data.message);
+  },
   methods: {
     barBack() {
       this.$navigateTo(Home, {
